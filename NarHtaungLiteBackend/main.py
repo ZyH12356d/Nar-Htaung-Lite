6f6222b44d61bef5084de02b20cf4d875b35697a
Zayar Htoo <107683128+ZyH12356d@users.noreply.github.com>
2026-02-06 11:44:04 +0630

    add requirments file

--- NarHtaungLiteBackend/main.py
+++ NarHtaungLiteBackend/main.py
@@ -225,6 +225,7 @@ def available_resolutions():
         return jsonify({"error": str(e)}), 500
     
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run()
+    #  app.run(debug=True)
+    # app.run(host="0.0.0.0", port=5000, debug=True)
 
